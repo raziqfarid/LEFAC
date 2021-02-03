012c4d71762b44346fc75a7fa2d682d06db4917f
Anthony Ilinykh <anthonyilinykh@gmail.com>
2021-02-03 16:50:29 +0300

    Using FeedMapper for all JSON unwrapping stuff

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -21,11 +21,44 @@ public final class RemoteFeedLoader: FeedLoader {
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
 		client.get(from: url) { result in
 			switch result {
-			case .success:
-				completion(.failure(RemoteFeedLoader.Error.invalidData))
+			case .success(let (data, response)):
+				do {
+					let items = try FeedMapper.map(data, response)
+					completion(.success(items))
+				} catch {
+					completion(.failure(RemoteFeedLoader.Error.invalidData))
+				}
 			case .failure:
 				completion(.failure(RemoteFeedLoader.Error.connectivity))
 			}
 		}
+	}
+}
+
+private class FeedMapper {
+	private struct Root: Decodable {
+		let items: [Item]
+	}
+	
+	private struct Item: Decodable {
+		let image_id: UUID
+		let image_desc: String?
+		let image_loc: String?
+		let image_url: URL
+		
+		var item: FeedImage {
+			return FeedImage(id: image_id, description: image_desc, location: image_loc, url: image_url)
+		}
+	}
+	
+	static var OK_200: Int { 200 }
+	
+	static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedImage] {
+		guard response.statusCode == OK_200 else {
+			throw RemoteFeedLoader.Error.invalidData
+		}
+		
+		let root = try JSONDecoder().decode(Root.self, from: data)
+		return root.items.map{ $0.item }
 	}
 }
