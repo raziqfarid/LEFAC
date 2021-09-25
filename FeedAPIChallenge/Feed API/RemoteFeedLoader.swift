3c2c3428bba507025e779f25dd011db915d8bb56
Rasik <Rasik>
2021-09-25 12:18:38 +0530

    System delivers connectivity error when client ends with an error,  by invoking 'completion' closure with 'Connectivity' Error in 'RemoteFeedLoader'

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -19,8 +19,8 @@ public final class RemoteFeedLoader: FeedLoader {
 	}
 	
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
-		client.get(from: url) { (_) in
-			
+		client.get(from: url) { (result) in
+			completion(.failure(Error.connectivity))
 		}
 	}
 }
