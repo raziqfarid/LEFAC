00c47979bafeac88e1411874385e854a6fdc9360
Rasik <Rasik>
2021-09-25 14:44:41 +0530

    System delivers Success with Items on 200 HTTP Response with JSON items, by invoking 'completion' closure using decode data. Separate decodable type is used for 'RemoteFeedLoader', to prevent the coding keys getting into other modules.

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -22,8 +22,12 @@ public final class RemoteFeedLoader: FeedLoader {
 		client.get(from: url) { (result) in
 			switch result {
 			case let .success((data, response)):
-				if response.statusCode == 200, let _ = try? JSONSerialization.jsonObject(with: data)  {
-					completion(.success([]))
+				if response.statusCode == 200, let rootData = try? JSONDecoder().decode(Root.self, from: data) {
+					completion(.success(
+						rootData.items.map({ (apiFeed) -> FeedImage in
+							FeedImage(id: apiFeed.id, description: apiFeed.description, location: apiFeed.location, url: apiFeed.url)
+						})
+					))
 				} else {
 					completion(.failure(Error.invalidData))
 				}
@@ -31,5 +35,31 @@ public final class RemoteFeedLoader: FeedLoader {
 			}
 			
 		}
+	}
+}
+
+fileprivate struct Root: Decodable {
+	fileprivate let items: [APIFeedImage]
+}
+
+fileprivate struct APIFeedImage: Decodable {
+	fileprivate let id: UUID
+	fileprivate let description: String?
+	fileprivate let location: String?
+	fileprivate let url: URL
+	
+	
+	fileprivate init(id: UUID, description: String?, location: String?, url: URL) {
+		self.id = id
+		self.description = description
+		self.location = location
+		self.url = url
+	}
+	
+	private enum CodingKeys: String, CodingKey {
+		case id = "image_id"
+		case description = "image_desc"
+		case location = "image_loc"
+		case url = "image_url"
 	}
 }
