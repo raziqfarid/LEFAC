e3ce9f614ca378ecd0841a3f6fd899e76086533a
Rasik <Rasik>
2021-09-25 15:07:50 +0530

    Moved 'mapDataToResult'  and it's associated data type to separate 'FeedMapper' class

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -23,50 +23,53 @@ public final class RemoteFeedLoader: FeedLoader {
 			guard let _ = self else { return }
 			switch result {
 			case let .success((data, response)):
-				completion(RemoteFeedLoader.mapDataToResult(data: data, response: response))
+				completion(FeedMapper.mapDataToResult(data: data, response: response))
 			case .failure: completion(.failure(Error.connectivity))
 			}
 		}
 	}
-	
-	private static func mapDataToResult(data: Data, response: HTTPURLResponse) -> FeedLoader.Result{
+}
+
+private final class FeedMapper {
+	fileprivate struct Root: Decodable {
+		fileprivate let items: [APIFeedImage]
+		
+		fileprivate var feedImages: [FeedImage] {
+			items.map { $0.feedImage }
+		}
+	}
+
+	fileprivate struct APIFeedImage: Decodable {
+		fileprivate let id: UUID
+		fileprivate let description: String?
+		fileprivate let location: String?
+		fileprivate let url: URL
+		
+		fileprivate var feedImage: FeedImage {
+			FeedImage(id: id, description: description, location: location, url: url)
+		}
+		
+		fileprivate init(id: UUID, description: String?, location: String?, url: URL) {
+			self.id = id
+			self.description = description
+			self.location = location
+			self.url = url
+		}
+		
+		private enum CodingKeys: String, CodingKey {
+			case id = "image_id"
+			case description = "image_desc"
+			case location = "image_loc"
+			case url = "image_url"
+		}
+	}
+
+	internal static func mapDataToResult(data: Data, response: HTTPURLResponse) -> FeedLoader.Result{
 		if response.statusCode == 200, let rootData = try? JSONDecoder().decode(Root.self, from: data) {
 			return .success(rootData.feedImages)
 		} else {
-			return .failure(Error.invalidData)
+			return .failure(RemoteFeedLoader.Error.invalidData)
 		}
 	}
 }
 
-fileprivate struct Root: Decodable {
-	fileprivate let items: [APIFeedImage]
-	
-	fileprivate var feedImages: [FeedImage] {
-		items.map { $0.feedImage }
-	}
-}
-
-fileprivate struct APIFeedImage: Decodable {
-	fileprivate let id: UUID
-	fileprivate let description: String?
-	fileprivate let location: String?
-	fileprivate let url: URL
-	
-	fileprivate var feedImage: FeedImage {
-		FeedImage(id: id, description: description, location: location, url: url)
-	}
-	
-	fileprivate init(id: UUID, description: String?, location: String?, url: URL) {
-		self.id = id
-		self.description = description
-		self.location = location
-		self.url = url
-	}
-	
-	private enum CodingKeys: String, CodingKey {
-		case id = "image_id"
-		case description = "image_desc"
-		case location = "image_loc"
-		case url = "image_url"
-	}
-}
